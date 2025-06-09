f56dc41618fb1863a6f46ecb8e922794b144a953
Amaan-Rajput <amaanrajput2236@gmail.com>
2025-06-09 19:20:04 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,118 @@
+const Pokemon = [
+    {
+        id: "1",
+        image: "./image/Pikachu.svg",
+        PokemonName: "Pikachu",
+        About: 'Pikachu—the iconic Electric type Pokémon—is more than a mascot. With its lightning-bolt tail and cheerful "Pika-Pika!", it symbolizes adventure and friendship. Ash loyal partner, Pikachu fights with Thunderbolts but wins hearts with playfulness, loyalty, and love for ketchup. Its true power? The bonds it creates. ⚡'
+    },
+    {
+        id: "2",
+        image: "./image/Lugia.png",
+        PokemonName: "Lugia",
+        About: 'Lugia the silver-winged legend who commands both sea and sky. Sleeping in ocean depths, this Psychic/Flying titan can calm storms or unleash devastating Aeroblast attacks. A majestic guardian balancing power and peace, its haunting song echoes across the waves as it watches over the Pokémon world.'
+    },
+    {
+        id: "3",
+        image: "./image/Bulbasaur.svg",
+        PokemonName: "Bulbasaur",
+        About: 'Bulbasaur the sturdy Grass Poison starter with a growing plant on its back. Gentle but tough, it soaks up sun for powerful attacks like Solar Beam, proving even #001 can be a powerhouse. A perfect mix of cute and strong'
+    },
+    {
+        id: "4",
+        image: "./image/Squirtle.svg",
+        PokemonName: "Squirtle",
+        About: 'Squirtle the adorable Water type starter, packs serious power in its tiny shell. With its signature sunglasses-cool attitude and water cannon attacks like Hydro Pump, this tiny turtle proves that big threats come in small packages. Whether blasting foes with powerful jets or retreating into its protective shell, Squirtle balances playful mischief with battle-ready toughness.'
+    },
+    {
+        id: "5",
+        image: "./image/Charizard.svg",
+        PokemonName: "Charizard",
+        About: 'Charizard the fiery dragon of your childhood dreams. With wings that scorch the sky and a roar that shakes mountains, this Flame Pokémon turns battles into infernos. Though stubborn, its loyalty burns as hot as its Blue Flare when fighting for a trusted Trainer. From Ash’s rebellious partner to Champion-tier powerhouse, Charizard proves some flames never fade—they just evolve. 🔥🐉'
+    },
+    {
+        id: "6",
+        image: "./image/Onix.svg",
+        PokemonName: "Onix",
+        About: 'Onix the living avalanche of rock and rage! This serpentine boulder crashes through terrain with earth-shaking force, its body grinding mountains to dust. Though its size chills opponents, a well-trained Onix moves with surprising speed just don’t get crushed by its Iron Tail. From Brock’s loyal guardian to underground terror, this Rock Snake proves even the earth itself can strike back'
+    },
+    {
+        id: "7",
+        image: "./image/Cubone.svg",
+        PokemonName: "Cubone",
+        About: 'Cubone the lonely warrior with a heart as heavy as its bone club. Wearing the skull of its lost mother, this Ground type Pokémon fights through tears, turning grief into fierce strength. that sorrow fuels brutal Bonemerang attacks. A tragic hero wrapped in mystery, Cubone teaches us that even the deepest pain can forge unbreakable courage'
+    },
+    {
+        id: "8",
+        image: "./image/Ninetales.svg",
+        PokemonName: "Ninetales",
+        About: 'Ninetales the elegant enigma wrapped in flames and folklore. With nine mystical tails and eyes that gleam with ancient wisdom, this Fire type embodies both beauty and vengeance. From dazzling Will-O-Wisps to scorching Infernos, Ninetales proves that true power moves as gracefully as flickering firelight.'
+    },
+    {
+        id: "9",
+        image: "./image/Dragonite.svg",
+        PokemonName: "Dragonite",
+        About: 'Dragonite the gentle giant with the heart of a hero and the might of a storm. This Dragon/Flying type soars across oceans at Mach speed. Behind its round belly and goofy grin hides a Hyper Beam that can level mountains. Proof that true strength doesn’t need to roar... until it absolutely must.'
+    },
+    {
+        id: "10",
+        image: "./image/Arcanine.svg",
+        PokemonName: "Arcanine",
+        About: "Arcanine the Legendary Pokémon that wasn't! With flames rippling through its golden fur and speed that rivals the wind, this Fire type embodies noble strength. Loyal to its core, it charges into battle with Extreme Speed and a roar that shakes the battlefield.Arcanine proves why it's revered as both a warrior and a legend in its own right."
+    },
+    {
+        id: "11",
+        image: "./image/Mr. Mime.svg",
+        PokemonName: "Mr. Mime",
+        About: 'Mr. Mime the unsettling yet mesmerizing master of invisible walls. With pantomime precision, this Psychic/Fairy type creates barriers out of thin air, turning battles into its own bizarre theater. Its creepy smile hides either genius or mischief (or both), and those who underestimate its Reflect or Psychic pay the price.'
+    },
+    {
+        id: "12",
+        image: "./image/Pidgeot.svg",
+        PokemonName: "Pidgeot",
+        About: "Pidgeot the sky's majestic sovereign with wings that stir hurricanes. This Normal/Flying type rules the clouds with razor-sharp feathers and a dive speed that shreds the air itself. trusted Trainer earns its loyalty—and the devastating force of its Hurricane attacks. , Pidgeot proves true royalty wears talons, not a crown."
+    },
+    {
+        id: "13",
+        image: "./image/Butterfree.svg",
+        PokemonName: "Butterfree",
+        About: 'Butterfree the gentle storm with rainbow wings. This Bug/Flying type dances on breezes, scattering sleep inducing pollen or devastating hurricanes with equal grace. From its humble start as a Caterpie to this floating marvel, Butterfree proves that true strength wears velvet wings'
+    },
+    {
+        id: "14",
+        image: "./image/Shiny Alakazam.svg",
+        PokemonName: "Shiny Alakazam",
+        About: 'Shiny Alakazam—the golden mind-warrior bending reality with a flick of its spoons. This Psychic type metallic sheen isn’t just for show; it reflects the blinding brilliance of an IQ over 5,000.its battles feel like psychic chess—checkmate before you even move. Rare, radiant, and ruthlessly clever, this is no mere spoon-bender… it’s a universe-bender.'
+    },
+    {
+        id: "15",
+        image: "./image/starmie fire red.svg",
+        PokemonName: "starmie fire red",
+        About: 'Starmie in FireRed the spinning enigma of the sea that baffles foes with its cosmic power. This Water/Psychic type moves like a living ruby, it’s a late game powerhouse that shreds through Team Rocket and the Elite Four alike. Starmie proves that the ocean’s depths hide the deadliest stars'
+    },
+    {
+        id: "16",
+        image: "./image/Beedrill.svg",
+        PokemonName: "Beedrill",
+        About: "Beedrill the sky's furious sting, striking with venomous precision. This Bug/Poison type moves in a blur of aggression, its twin drills laced with lethal Toxin. Once a humble Weedle, now a swarm's wrath incarnate—cross its path, and pray you're faster than its Mega-Evolved fury"
+    },
+]
+let number = 1;
+
+const mainCircle = document.getElementsByClassName('center-circle')[0];
+const littleCircle = document.querySelectorAll('.small-circle');
+Array.from(document.getElementsByClassName('small-circle')).forEach((e) => {
+    e.addEventListener('click', (el) => {
+        number = el.target.id;
+
+        let circle = Pokemon.filter((re) => {
+            return re.id == number;
+        });
+        circle.forEach(ret => {
+            let { image, PokemonName, About } = ret;
+            mainCircle.innerHTML = `<h2>${PokemonName}</h2>
+            <img src="${image}">
+            <p>${About}</p>`;
+        });
+
+    })
+});
